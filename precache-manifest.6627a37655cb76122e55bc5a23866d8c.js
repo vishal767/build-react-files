b6b36f2b2e17f3ffa13054b686586ab8359dbc00
“vishal” <jsvishal2014@gmail.com>
2019-03-04 19:07:07 +0530

    push 0.1

---
+++ precache-manifest.6627a37655cb76122e55bc5a23866d8c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1575c1475c6ec767665c",
+    "url": "/static/css/main.86748947.chunk.css"
+  },
+  {
+    "revision": "1575c1475c6ec767665c",
+    "url": "/static/js/main.1575c147.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "f8247bbe37f33638aad2",
+    "url": "/static/css/2.417c1618.chunk.css"
+  },
+  {
+    "revision": "f8247bbe37f33638aad2",
+    "url": "/static/js/2.f8247bbe.chunk.js"
+  },
+  {
+    "revision": "8faee0746cc17a9a4ecc2891a7817663",
+    "url": "/static/media/custom-background.8faee074.png"
+  },
+  {
+    "revision": "7333e0e975677de957c8aa67cd4e0b48",
+    "url": "/index.html"
+  }
+];
